0b2d2e47f1131c4b0a187b1bb2bd8c02e1b85783
barinbritva <barinbritva@outlook.com>
2026-06-09 17:03:31 +0700

    Setup Capacitor dev mode.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,23 @@
 import type {CapacitorConfig} from '@capacitor/cli';
+import {env} from './env';
+
+const isDev = env.nodeEnv === 'development';
+// In dev we use protocol from provided dev server URL, in prod we default to https
+const scheme = isDev ? new URL(env.devServerUrl).protocol.replace(':', '') : 'https';
 
 const config: CapacitorConfig = {
 	appId: 'com.example.app',
 	appName: 'frontend',
 	webDir: 'dist',
+	server: {
+		androidScheme: scheme,
+		// In dev we use provided dev server URL instead of using physical built files from dist folder
+		// enabling live reload and other dev features
+		...(isDev && {
+			url: env.devServerUrl,
+			cleartext: true,
+		}),
+	},
 };
 
 export default config;
--- env.ts
+++ env.ts
@@ -1,4 +1,7 @@
+const devServerPort = process.env['DEV_SERVER_PORT'] ? Number(process.env['DEV_SERVER_PORT']) : 5173;
+
 export const env = {
 	nodeEnv: process.env['NODE_ENV'] ?? 'production',
-	devServerPort: process.env['DEV_SERVER_PORT'] ? Number(process.env['DEV_SERVER_PORT']) : 5173,
+	devServerPort,
+	devServerUrl: process.env['DEV_SERVER_EXTERNAL_URL'] || `http://localhost:${devServerPort}`,
 } as const;
