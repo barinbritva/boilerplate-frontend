e25c739f5f8bfc08853ce9a8b31138a5ecc2fde7
barinbritva <barinbritva@outlook.com>
2026-08-04 20:45:39 +0700

    Heading to support 1-6 levels.

--- src/ui/atoms/Heading.tsx
+++ src/ui/atoms/Heading.tsx
@@ -1,10 +1,12 @@
 import {type ComponentType, type HTMLAttributes} from 'react';
 
-export interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {}
+export interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
+	level: 1 | 2 | 3 | 4 | 5 | 6;
+}
 
-export const Heading: ComponentType<HeadingProps> = ({children, ...props}) => {
-	return (
-		// todo allow different heading levels
-		<h1 {...props}>{children}</h1>
-	);
+const tags = {1: 'h1', 2: 'h2', 3: 'h3', 4: 'h4', 5: 'h5', 6: 'h6'} as const;
+
+export const Heading: ComponentType<HeadingProps> = ({children, level, ...props}) => {
+	const Tag = tags[level];
+	return <Tag {...props}>{children}</Tag>;
 };
